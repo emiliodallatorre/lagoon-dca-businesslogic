e2f68aef7b9f2cd42b19c46c596e461004e986be
Emilio Dalla Torre <info@emiliodallatorre.it>
2025-06-26 14:29:54 +0200

    Created project

---
+++ src/main/java/it/lagoon/dca/businesslogic/LagoonDcaBusinesslogicApplication.java
@@ -0,0 +1,13 @@
+package it.lagoon.dca.businesslogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LagoonDcaBusinesslogicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LagoonDcaBusinesslogicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/lagoon/dca/businesslogic/LagoonDcaBusinesslogicApplicationTests.java
@@ -0,0 +1,13 @@
+package it.lagoon.dca.businesslogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LagoonDcaBusinesslogicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
